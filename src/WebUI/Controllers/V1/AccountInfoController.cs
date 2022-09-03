f1dda478be689db2f724fa596d135b3a4caf1b4f
Danil <47692381+SelDanilEv@users.noreply.github.com>
2022-09-03 17:30:24 +0400

    Update AccountInfoController.cs

--- src/WebUI/Controllers/V1/AccountInfoController.cs
+++ src/WebUI/Controllers/V1/AccountInfoController.cs
@@ -1,9 +1,10 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Defender.MoneyTracking.Application.Models.LoginResponse;
 using Defender.MoneyTracking.Application.Modules.Auth.Commands;
 using Defender.MoneyTracking.Application.DTOs;
+using Defender.MoneyTracking.WebUI.Attributes;
+using Defender.MoneyTracking.Domain.Models;
 
 namespace Defender.MoneyTracking.WebUI.Controllers.V1;
 
@@ -14,6 +15,7 @@ public class AccountInfoController : BaseApiController
     }
 
     [HttpPut("update")]
+    [Auth(Roles.Any)]
     [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<UserDto> UpdateAccountInfoAsync(
